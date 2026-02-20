3b2b6d2f567379a34f7d2defc9c9ac7b553c5ec2
Malware Testing <malwaretestinginfo@gmail.com>
2026-02-20 17:02:04 +0100

    Add legacy Vercel now-build compatibility settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & { target?: "serverless" } = {
   typedRoutes: true
 };
+
+if (process.env.NOW_BUILDER === "1") {
+  nextConfig.target = "serverless";
+}
 
 export default nextConfig;
